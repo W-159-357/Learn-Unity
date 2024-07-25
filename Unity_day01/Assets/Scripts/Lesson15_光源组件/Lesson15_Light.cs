bc1f7ab498f9cc0d7231cecef59eb6db2846f157
wxj <2574762712@qq.com>
2024-07-25 23:26:08 +0800

    add: Light

---
+++ Unity_day01/Assets/Scripts/Lesson15_光源组件/Lesson15_Light.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson15_Light : MonoBehaviour
+{
+    public Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region 一、面板参数
+
+        #endregion
+
+        #region 二、代码控制
+        light.intensity = 2.7f;
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
